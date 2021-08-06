910c416bdea643576d32fd7450e8880124a55e1f
kangdoohyun <rkdengus1208@gmail.com>
2021-08-06 19:05:30 +0900

    커밋

---
+++ src/main/java/com/jhs/exam/exam2/http/controller/AdmMemberController.java
@@ -0,0 +1,74 @@
+package com.jhs.exam.exam2.http.controller;
+
+import com.jhs.exam.exam2.container.Container;
+import com.jhs.exam.exam2.dto.Member;
+import com.jhs.exam.exam2.dto.ResultData;
+import com.jhs.exam.exam2.http.Rq;
+import com.jhs.exam.exam2.service.MemberService;
+import com.jhs.exam.exam2.util.Ut;
+
+public class AdmMemberController extends Controller {
+	private MemberService memberService;
+
+	public void init() {
+		memberService = Container.memberService;
+	}
+
+	@Override
+	public void performAction(Rq rq) {
+		switch (rq.getActionMethodName()) {
+		case "login":
+			actionShowLogin(rq);
+			break;
+		case "doLogin":
+			actionDoLogin(rq);
+			break;
+		case "doLogout":
+			actionDoLogout(rq);
+			break;
+		default:
+			rq.println("존재하지 않는 페이지 입니다.");
+			break;
+		}
+	}
+
+	private void actionDoLogout(Rq rq) {
+		rq.removeSessionAttr("loginedMemberJson");
+		rq.replace(null, "../../");
+	}
+
+	private void actionDoLogin(Rq rq) {
+		String loginId = rq.getParam("loginId", "");
+		String loginPw = rq.getParam("loginPw", "");
+
+		if (loginId.length() == 0) {
+			rq.historyBack("loginId를 입력해주세요.");
+			return;
+		}
+
+		if (loginPw.length() == 0) {
+			rq.historyBack("loginPw를 입력해주세요.");
+			return;
+		}
+
+		ResultData loginRd = memberService.admLogin(loginId, loginPw);
+
+		if (loginRd.isFail()) {
+			rq.historyBack(loginRd.getMsg());
+			return;
+		}
+
+		Member member = (Member) loginRd.getBody().get("member");
+
+		rq.setSessionAttr("loginedMemberJson", Ut.toJson(member, ""));
+
+		String redirectUri = rq.getParam("redirectUri", "../home/main");
+
+		rq.replace(loginRd.getMsg(), redirectUri);
+	}
+
+	private void actionShowLogin(Rq rq) {
+		rq.jsp("adm/member/login");
+	}
+
+}
